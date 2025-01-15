b344c57487cd924029f879c435e12836cbfe57c0
maayan <maayan@gmail.com>
2025-01-15 17:16:25 +0200

    Create layout component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,7 +2,6 @@ import { Grid, GridItem, Image, Show } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import GameHeader from "../components/GameHeader";
-import NavBar from "../components/NavBar";
 import TrackGrid from "../components/TrackGrid";
 import { Track } from "../entities/Track";
 import useGame from "../hooks/useGame";
@@ -34,18 +33,14 @@ function GameDetailsPage() {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "main aside"`,
+        base: `"main"`,
+        lg: `"main aside"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "1fr 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar onSearch={(searchText) => console.log(searchText)} />
-      </GridItem>
-
       <GridItem area="main" paddingX={35}>
         {data && <GameHeader data={data} />}
         <TrackGrid topTracks={topTracks} loading={loadingTracks} />
