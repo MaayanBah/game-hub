87cdf56c051b922a5fd1f3a396ac021e2be4a820
MaayanBah <maayanbah@gmail.com>
2025-01-15 17:16:25 +0200

    Create layout component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -26,17 +26,14 @@ function HomePage() {
   return (
     <Grid
       templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
+        base: `"main"`,
+        lg: `"aside main"`,
       }}
       templateColumns={{
         base: "1fr",
         lg: "200px 1fr",
       }}
     >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
           <GenreList />
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
+import Layout from "./pages/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Layout />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:slug", element: <GameDetailPage /> },
